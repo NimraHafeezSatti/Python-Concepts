f3725228fece254deda7075736e00f9c8fc4e806
Nimra-Python-Developer <nimrahafeez2007@gmail.com>
2024-06-30 00:48:42 +0500

    math functions max function

--- index.py
+++ index.py
@@ -8,11 +8,17 @@
 # for loops
 # sets
 # user input3
-name=input("what is you name:")
-age=int(input("how old are you:"))
-height=float(input("what is you height: "))
-age=age+1
-height=height+0.5
-print("hello"+"  "+name)
-print("you are"+str(age)+"years old")
-print("my height is "+str(height)+" cm")
+# name=input("what is you name:")
+# age=int(input("how old are you:"))
+# height=float(input("what is you height: "))
+# age=age+1
+# height=height+0.5
+# print("hello"+"  "+name)
+# print("you are"+str(age)+"years old")
+# print("my height is "+str(height)+" cm")
+#math functions max function
+
+x=2
+y=5
+z=3
+print(max(x,y,z))
