b6f10b2a1f4bb72227c1f224615de5a84d1abdb6
Nimra-Python-Developer <nimrahafeez2007@gmail.com>
2024-06-30 01:49:13 +0500

    string slicing

--- index.py
+++ index.py
@@ -6,18 +6,7 @@
 #2dlists
 # tuples
 # for loops
-# sets
-# user input3
-# name=input("what is you name:")
-# age=int(input("how old are you:"))
-# height=float(input("what is you height: "))
-# age=age+1
-# height=height+0.5
-# print("hello"+"  "+name)
-# print("you are"+str(age)+"years old")
-# print("my height is "+str(height)+" cm")
-#math functions min function
-x=2 
-y=5
-z=3
-print(min(x,y,z))
+# string slicing
+name="bro code"
+first_name=name[0:3]
+print(first_name)
