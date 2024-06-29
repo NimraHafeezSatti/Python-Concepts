82067bbb9d7666b70b4bc20b620dc43485771c71
Nimra-Python-Developer <nimrahafeez2007@gmail.com>
2024-06-30 00:25:02 +0500

    user input3

--- index.py
+++ index.py
@@ -7,9 +7,12 @@
 # tuples
 # for loops
 # sets
-# user input2
+# user input3
 name=input("what is you name:")
-age=float(input("how old are you:"))
+age=int(input("how old are you:"))
+height=float(input("what is you height: "))
 age=age+1
+height=height+0.5
 print("hello"+"  "+name)
 print("you are"+str(age)+"years old")
+print("my height is "+str(height)+" cm")
