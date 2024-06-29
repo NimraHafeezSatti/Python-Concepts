10dde8a947c59369c964a7a4fd7557a3d4480429
Nimra-Python-Developer <nimrahafeez2007@gmail.com>
2024-06-29 20:22:43 +0500

    lists2

--- index.py
+++ index.py
@@ -1,8 +1,15 @@
 # #first code
 # print('I like to eat pizza')
 #lists
+# food=["pizza","tea","cheese","orange"]
+# print(food[1])
+#lists2
 food=["pizza","tea","cheese","orange"]
-print(food[1])
+food[1]="fish"
+# print(food[1])
+for x in food :
+    print(x)
+
 
 
 
