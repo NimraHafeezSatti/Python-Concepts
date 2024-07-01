652471fe4df00955cb9ef57046d697fcf82f97bf
Nimra-Python-Developer <nimrahafeez2007@gmail.com>
2024-07-01 19:20:34 +0500

    exception

--- index.py
+++ index.py
@@ -3,14 +3,30 @@
 # first_name=name[:3]
 # print(first_name)
 #function
-def hello():
-    print("hello!")
+# def hello():
+#     print("hello!")
 
 
 
 
 
 
-hello()
-hello()
-hello()
+# hello()
+# hello()
+# hello()
+#exception
+try:
+    numerator=int(input("divide number by:"))
+    denominator=int(input("divide  number by:"))
+    result=numerator/denominator
+except ZeroDivisionError:
+     print("idiot!you can't divide it by zero!")
+
+except Exception:
+    print("something went wrong!")
+except ValueError:
+     print("you entered invalid input! please enter a number.")
+else:
+    print(result)
+finally:
+    print("This is always executed.")
