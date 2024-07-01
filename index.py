c894cc78b74643851776e91e1d2cf57b86383937
Nimra-Python-Developer <nimrahafeez2007@gmail.com>
2024-07-01 18:55:58 +0500

    function

--- index.py
+++ index.py
@@ -1,12 +1,16 @@
-# #first code
-# print('I like to eat pizza')
-#lists
-# food=["pizza","tea","cheese","orange"]
-# print(food[1])
-#2dlists
-# tuples
-# for loops
-# string slicing
-name="bro code"
-first_name=name[0:3]
-print(first_name)
+# #slicing
+# name="bro code"
+# first_name=name[:3]
+# print(first_name)
+#function
+def hello():
+    print("hello!")
+
+
+
+
+
+
+hello()
+hello()
+hello()
